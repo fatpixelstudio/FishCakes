b4a522448b5a467ca4d04da33920f27a000107b9
Marijn Tijhuis <post@fatpixel.nl>
2020-08-05 09:35:53 +0200

    Bit more styling and markup for the user options.

--- dist/fishcakes.js
+++ dist/fishcakes.js
@@ -41,9 +41,12 @@ var cookie = {
 
 var fishcakesbanner = document.querySelector('.js-fishcakes');
 var fishcakesbutton = document.querySelector('.js-fishcakes-close');
+var fishcakesoptions = document.querySelector('.js-fishcakes-options');
 
 if (cookie.get('hide_cookie_banner') === 'yes') {
 	fishcakesbanner.classList.add('is-hidden');
+	// if(navigator.doNotTrack)
+
 } else {
 	fishcakesbanner.classList.add('is-active');
 }
@@ -54,4 +57,13 @@ function closeBanner() {
 	cookie.set('hide_cookie_banner', 'yes', 365);
 }
 
+function consentScript() {
+	console.log('i give my consent');
+	var scripts = document.querySelectorAll('.fishcakes-consent-script');
+	for(i = 0; i < scripts.length; i++) {
+		scripts[i].setAttribute('type', 'text/javascript');
+	}
+}
+
+fishcakesbutton.addEventListener('click', consentScript, false);
 fishcakesbutton.addEventListener('click', closeBanner, false);
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -13,4 +13,13 @@ function closeBanner() {
 	cookie.set('hide_cookie_banner', 'yes', 365);
 }
 
+function consentScript() {
+	console.log('i give my consent');
+	var scripts = document.querySelectorAll('.fishcakes-consent-script');
+	for(i = 0; i < scripts.length; i++) {
+		scripts[i].setAttribute('type', 'text/javascript');
+	}
+}
+
+fishcakesbutton.addEventListener('click', consentScript, false);
 fishcakesbutton.addEventListener('click', closeBanner, false);
